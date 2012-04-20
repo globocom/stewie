b271c5a8883bcc6670ed98cf697e6b79a27b3e30
Juarez Bochi <jbochi@gmail.com>
2012-04-20 01:24:32 -0300

    Removes fms_collector

--- fms_collector/client.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def post(stewie_server, bucket, target, timestamp=None, **kwargs):
-    url = '/'.join([stewie_server, 'api/metrics', bucket, target])
-    requests.post(url, data=kwargs)
