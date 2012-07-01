78e22852c547d7f56758a5b6b5a7fe383213a81f
Juarez Bochi <jbochi@gmail.com>
2012-07-01 19:49:12 -0300

    Web app detector

--- src/stewie/app.clj
+++ src/stewie/app.clj
@@ -1,12 +1,25 @@
 (ns stewie.app
+  (:use stewie.core)
   (:use noir.core)
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+      [noir.response :as response]))
+
+(def detect (detector))
 
 (defpage "/" []
-    "Welcome to Stewie!")
+  "Welcome to Stewie!")
+
+(defn convert-to-numbers
+  "convert hash-map values to numbers"
+  [h]
+  (into {} (map (fn [[k v]] [k (read-string v)]) h)))
+
+(defpage [:post "/"] {:as data}
+  (let [data (convert-to-numbers data)
+        data (assoc data :density (detect data))]
+    (response/json data)))
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (server/start port {:mode mode
-                        :ns 'my-noir})))
+    (server/start port {:mode mode :ns 'my-noir})))
