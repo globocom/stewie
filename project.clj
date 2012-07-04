335c4f28fbc2d4765177b1e594a2c37b079f32aa
Juarez Bochi <jbochi@gmail.com>
2012-07-04 15:06:07 -0300

    Fix dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject stewie "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lein-noir "1.2.1"]]
+                 [noir "1.2.1"]]
   :dev-dependencies [[midje "1.4.0"]
-                     [lein-midje "1.0.9"]]
+                     [lein-midje "1.0.9"]
+                     [lein-noir "1.2.1"]]
   :main stewie.app)
