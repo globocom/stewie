9cde1a532b820d6859203f019efde26a54cd1eae
Juarez Bochi <jbochi@gmail.com>
2012-07-05 09:16:26 -0300

    Add lazy-test dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
                  [noir "1.2.1"]]
   :dev-dependencies [[midje "1.4.0"]
                      [lein-midje "1.0.9"]
-                     [lein-noir "1.2.1"]]
+                     [lein-noir "1.2.1"]
+                     [com.stuartsierra/lazytest "1.2.3"]]
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :main stewie.app)
