817f2c5bc1bcda88a5793519841034072a08e413
Juarez Bochi <jbochi@gmail.com>
2012-07-05 16:59:12 -0300

    Testing api returned valued

--- test/stewie/test/app.clj
+++ test/stewie/test/app.clj
@@ -1,6 +1,7 @@
 (ns stewie.test.app
   (:use [stewie.app])
   (:use [midje.sweet])
+  (:require [cheshire.core :as json])
   (:require [noir.util.test :as noir]))
 
 (fact "homepage returns status 200"
@@ -9,4 +10,5 @@
 (fact "post to bucket returns json"
   (let [response (noir/send-request [:post "/bucket"])]
     (response :status) => 200
-    ((response :headers) "Content-Type") => "application/json"))
+    ((response :headers) "Content-Type") => "application/json"
+    ((json/parse-string (response :body)) "density") => 1))
