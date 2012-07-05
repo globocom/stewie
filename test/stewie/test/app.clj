dfc2133318a84769a659677acd8538c699860ee3
Juarez Bochi <jbochi@gmail.com>
2012-07-05 17:25:57 -0300

    Testing API memory

--- test/stewie/test/app.clj
+++ test/stewie/test/app.clj
@@ -1,5 +1,6 @@
 (ns stewie.test.app
   (:use [stewie.app])
+  (:use [stewie.core])
   (:use [midje.sweet])
   (:require [cheshire.core :as json])
   (:require [noir.util.test :as noir]))
@@ -8,7 +9,15 @@
   (-> (noir/send-request "/") :status) => 200)
 
 (fact "post to bucket returns json"
-  (let [response (noir/send-request [:post "/bucket"])]
+  (let [response (noir/send-request [:post "/bucket"] {"x" "1"})]
     (response :status) => 200
     ((response :headers) "Content-Type") => "application/json"
-    ((json/parse-string (response :body)) "density") => 1))
+    ((json/parse-string (response :body)) "density") => "NaN"))
+
+(fact "post to bucket has memory"
+  (noir/with-noir
+    (let [input (range 11)
+          post-to-bucket (fn [x] (noir/send-request [:post "/memory"] {"x" (str x)}))
+          response (last (for [x input] (post-to-bucket x)))
+          json-response (json/parse-string (response :body))]
+      (json-response "density") => (density (last input) (average input) (variance input)))))
