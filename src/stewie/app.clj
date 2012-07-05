a7ac08a923a3f70d4121430fa9693fa3a3edcc88
Juarez Bochi <jbochi@gmail.com>
2012-07-05 09:41:55 -0300

    Fix app server namespace

--- src/stewie/app.clj
+++ src/stewie/app.clj
@@ -22,4 +22,4 @@
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (server/start port {:mode mode :ns 'my-noir})))
+    (server/start port {:mode mode :ns 'stewie})))
