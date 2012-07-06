0cffc6a5adaef10a929304e80d688735dfbfe6d3
Juarez Bochi <jbochi@gmail.com>
2012-07-06 16:00:38 -0300

    Fix db functions documentation

--- src/stewie/db.clj
+++ src/stewie/db.clj
@@ -4,14 +4,16 @@
 
 ;MongoDB setup functions taken from http://thecomputersarewinning.com/post/clojure-heroku-noir-mongo/
 
-(defn split-mongo-url [url]
+(defn split-mongo-url
   "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
+  [url]
   (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
     (when (.find matcher) ;; Check if it matches.
       (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
 
-(defn maybe-init [collection]
+(defn maybe-init
   "Checks if connection and collection exist, otherwise initialize."
+  [collection]
   (when (not (connection? *mongo-config*)) ;; If global connection doesn't exist yet.
     (let [mongo-url (get (System/getenv) "MONGOHQ_URL" "mongodb://:@localhost:27017/db") ;; Heroku puts it here.
           config    (split-mongo-url mongo-url)] ;; Extract options.
